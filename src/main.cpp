88973024badae0763cf05530b7aea052ca30cbfd
efondere <ef@eliotfondere.com>
2023-05-06 13:08:19 -0400

    Implement more Matrix methods

--- src/main.cpp
+++ src/main.cpp
@@ -25,13 +25,46 @@ float c_ij(size_t N, size_t i, size_t j)
 		* std::cos((static_cast<float>(i * (2 * j + 1)) * mat::pi) / static_cast<float>(2 * N) );
 }
 
-int main()
-{
+int main() {
 	const size_t N = 8;
 
-    mat::Matrix<N, N, float> m =
-			mat::Matrix<N, N, float>::FromFunction([](size_t i, size_t j){return c_ij(N, i, j);});
-	m.print();
+	mat::Matrix<N, N, float> C =
+			mat::Matrix<N, N, float>::FromFunction([](size_t i, size_t j) { return c_ij(N, i, j); });
+
+	float f_data[8][8] =
+		{
+			{40, 193, 89, 37, 209, 236, 41, 14},
+			{102, 165, 36, 150, 247, 104, 7, 19},
+			{157, 92, 88, 251, 156, 3, 20, 35},
+			{153, 75, 220, 193, 29, 13, 34, 22},
+			{116, 173, 240, 54, 11, 38, 20, 19},
+			{162, 255, 109, 9, 26, 22, 20, 29},
+			{237, 182, 5, 28, 20, 15, 28, 20},
+			{222, 33, 8, 23, 24, 29, 23, 23}
+		};
+
+	float q_data[8][8] =
+		{
+			{10, 16, 22, 28, 34, 40, 46, 52},
+			{16, 22, 28, 34, 40, 46, 52, 58},
+			{22, 28, 34, 40, 46, 52, 58, 64},
+			{28, 34, 40, 46, 52, 58, 64, 70},
+			{34, 40, 46, 52, 58, 64, 70, 76},
+			{40, 46, 52, 58, 64, 70, 76, 82},
+			{46, 52, 58, 64, 70, 76, 82, 88},
+			{52, 58, 64, 70, 76, 82, 88, 94}
+		};
+
+	mat::Matrix<N, N, float> f = mat::Matrix<N, N, float>::FromArray(f_data);
+	auto f_tilde = f - 128;
+	auto alpha_tilde = C.product((f_tilde.product(C.transpose())));
+
+
+	mat::Matrix<N, N, float> q = mat::Matrix<N, N, float>::FromArray(q_data);
+	auto l = alpha_tilde / q - (-0.5);
+	mat::Matrix<N, N, int> l_int = l.asType<int>([](float input){return static_cast<int>(floorf(input));});
+
+	l_int.print();
 
 	return 0;
 }
--- src/math/Matrix.hpp
+++ src/math/Matrix.hpp
@@ -20,12 +20,25 @@ namespace sqh
 namespace math
 {
 
+template<typename T, typename N_T>
+inline N_T default_conversion(T input)
+{
+	return static_cast<N_T>(input);
+}
+
 template<size_t R, size_t C, typename T>
 class Matrix
 {
 public:
+	struct Index
+	{
+		size_t i;
+		size_t j;
+	};
+
 	Matrix();
 	static Matrix<R, C, T> FromFunction(T(*function)(size_t, size_t));
+	static Matrix<R, C, T> FromArray(T array[R][C]);
 	static Matrix<R, C, T> Outer(Matrix<1, C, T> rows, Matrix<R, 1, T> columns);
 	static Matrix<R, C, T> Identity();
 	// static Transpose() -> or have it as a separate function?
@@ -33,6 +46,20 @@ public:
 	Matrix<R, 1, T> getColumn(size_t index);
 	Matrix<1, C, T> getRow(size_t index);
 	Matrix<C, 1, T> getRowT(size_t index);
+
+	std::array<T, R * C>&& flatten(Index(*flatten_function)(size_t));
+
+	template<typename N_T>
+	Matrix<C, R, N_T> asType(N_T(*conversion_function)(T input)=default_conversion);
+
+	Matrix<C, R, T> transpose();
+
+	Matrix<R, C, T> operator-(T scalar);
+	Matrix<R, C, T> operator*(T scalar);
+	Matrix<R, C, T> operator/(Matrix<R, C, T>& other);
+
+	template<size_t C_2>
+	Matrix<R, C_2, T> product(Matrix<C, C_2, T> other);
 
 	void print();
 
@@ -109,6 +136,200 @@ Matrix<R, C, T> Matrix<R, C, T>::FromFunction(T (*function)(size_t, size_t))
 		for (size_t j = 0; j < C; j++)
 		{
 			m.data[i][j] = function(i, j);
+		}
+	}
+
+	return m;
+}
+
+template<size_t R, size_t C, typename T>
+Matrix<R, C, T> Matrix<R, C, T>::Outer(Matrix<1, C, T> rows, Matrix<R, 1, T> columns)
+{
+	Matrix<R, C, T> m;
+
+	for (size_t i = 0; i < R; i++)
+	{
+		for (size_t j = 0; j < C; j++)
+		{
+			m.data[i][j] = columns.data[i][0] * rows.data[0][j];
+		}
+	}
+
+	return m;
+}
+
+template<size_t R, size_t C, typename T>
+Matrix<R, 1, T> Matrix<R, C, T>::getColumn(size_t index)
+{
+	Matrix<R, 1, T> m;
+
+	for (size_t i = 0; i < R; i++)
+	{
+		m.data[i][0] = data[i][index];
+	}
+
+	return m;
+}
+
+template<size_t R, size_t C, typename T>
+Matrix<1, C, T> Matrix<R, C, T>::getRow(size_t index)
+{
+	Matrix<1, C, T> m;
+
+	for (size_t i = 0; i < R; i++)
+	{
+		m.data[0][i] = data[index][i];
+	}
+
+	return m;
+}
+
+template<size_t R, size_t C, typename T>
+Matrix<C, 1, T> Matrix<R, C, T>::getRowT(size_t index)
+{
+	Matrix<1, C, T> m;
+
+	for (size_t i = 0; i < R; i++)
+	{
+		m.data[i][0] = data[index][i];
+	}
+
+	return m;
+}
+
+template<size_t R, size_t C, typename T>
+std::array<T, R * C>&& Matrix<R, C, T>::flatten(Matrix::Index (*flatten_function)(size_t)) {
+	static_assert(false, "flatten no implemented");
+	return std::array<T, R * C>();
+}
+
+template<size_t R, size_t C, typename T>
+Matrix<C, R, T> Matrix<R, C, T>::transpose() {
+	Matrix<C, R, T> m;
+
+	for (size_t i = 0; i < C; i++)
+	{
+		for (size_t j = 0; j < R; j++)
+		{
+			m.data[i][j] = data[j][i];
+		}
+	}
+
+	return m;
+}
+
+template<size_t R, size_t C, typename T>
+template<size_t C_2>
+Matrix<R, C_2, T> Matrix<R, C, T>::product(Matrix<C, C_2, T> other)
+{
+	Matrix<R, C_2, T> m;
+
+	for (size_t i = 0; i < R; i++)
+	{
+		for (size_t j = 0; j < C_2; j++)
+		{
+			T sum = 0;
+			for (size_t k = 0; k < C; k++)
+			{
+				sum += data[i][k] * other.data[k][j];
+			}
+
+			m.data[i][j] = sum;
+		}
+	}
+
+	return m;
+}
+
+template<size_t R, size_t C, typename T>
+Matrix<R, C, T> Matrix<R, C, T>::FromArray(T array[R][C]) {
+	Matrix<R, C, T> m;
+
+	for (size_t j = 0; j < C; j++)
+	{
+		for (size_t i = 0; i < R; i++)
+		{
+			m.data[i][j] = array[i][j];
+		}
+	}
+
+	return m;
+}
+
+template<size_t R, size_t C, typename T>
+Matrix<R, C, T> Matrix<R, C, T>::operator*(T scalar)
+{
+	Matrix<R, C, T> m;
+
+	for (size_t j = 0; j < C; j++)
+	{
+		for (size_t i = 0; i < R; i++)
+		{
+			m.data[i][j] = data[i][j] * scalar;
+		}
+	}
+
+	return m;
+}
+
+template<size_t R, size_t C, typename T>
+Matrix<R, C, T> Matrix<R, C, T>::operator-(T scalar)
+{
+	Matrix<R, C, T> m;
+
+	for (size_t j = 0; j < C; j++)
+	{
+		for (size_t i = 0; i < R; i++)
+		{
+			m.data[i][j] = data[i][j] - scalar;
+		}
+	}
+
+	return m;
+}
+
+template<size_t R, size_t C, typename T>
+Matrix<R, C, T> Matrix<R, C, T>::operator/(Matrix<R, C, T>& other) {
+	Matrix<R, C, T> m;
+
+	for (size_t j = 0; j < C; j++)
+	{
+		for (size_t i = 0; i < R; i++)
+		{
+			m.data[i][j] = data[i][j] / other.data[i][j];
+		}
+	}
+
+	return m;
+}
+
+template<size_t R, size_t C, typename T>
+template<typename N_T>
+Matrix<C, R, N_T> Matrix<R, C, T>::asType(N_T (*conversion_function)(T))
+{
+	Matrix<C, R, N_T> m;
+
+	for (size_t j = 0; j < C; j++)
+	{
+		for (size_t i = 0; i < R; i++)
+		{
+			m.data[i][j] = conversion_function(data[i][j]);
+		}
+	}
+
+	return m;
+}
+
+template<size_t R, size_t C, typename T>
+constexpr Matrix<R, C, T> operator*(T scalar, Matrix<R, C, T> matrix)
+{
+	Matrix<R, C, T> m;
+
+	for (size_t j = 0; j < C; j++)
+	{
+		for (size_t i = 0; i < R; i++)
+		{
+			m.data[i][j] = matrix.data[i][j] * scalar;
 		}
 	}
 
